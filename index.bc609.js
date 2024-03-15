dd119c51bee02f074ca6109de42309ac392a4e39
babyRess <139874554+babyRess@users.noreply.github.com>
2024-03-15 12:57:02 +0700

    update

--- index.bc609.js
+++ index.bc609.js
@@ -1,4 +1,4 @@
-System.register(["./application.f216b.js"], function (_export, _context) {
+System.register(["./application.74119.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.f216b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationF216bJs) {
-      Application = _applicationF216bJs.Application;
+    setters: [function (_application74119Js) {
+      Application = _application74119Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
